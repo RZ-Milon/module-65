e8dffeefca15eee05e89b3d3bb1b4f9d5a5949fa
rzmilon <rzmilon@gmail.com>
2022-12-07 23:58:18 +0600

    frist commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// user: dbUser2
+// password: cWmhfxkfvaigwn8z
+
+
+
+const uri = "mongodb+srv://dbUser2:cWmhfxkfvaigwn8z@cluster0.te4s04i.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const userCollection = client.db('nodeMongoCrud').collection('users');
+        const user ={
+            name: 'testing test',
+            email: 'testing@gmail.com'
+        };
+        const result = await userCollection.insertOne(user);
+
+        console.log(result);
+
+    }
+    finally{
+        
+    }
+}
+
+run().catch(err => console.log(err));
+
+
+
+
+app.get('/', (req, res)=>{
+    res.send('Hello from node mongo curd server');
+});
+
+
+app.listen(port, ()=>{
+    console.log(`Listing to port ${port}`);
+})
